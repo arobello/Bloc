ad45799f919079e9f460fa328361d0dfaba64eca
Ashley Robello <ashley@kabojo.com>
2018-08-27 15:31:57 -0700

    Checkpoint React To-Do: Events complete

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
-class ToDo extends Component {
-	render() {
-		return (
 
-			<li> A todo will go here </li>
-      
-		);
+	class ToDo extends Component {
+	  render() {
+	    return (
+	      <li>
+	        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+	        <span>{ this.props.description }</span>
+	      </li>
+	    );
+	  }
 	}
-}
 
-export default ToDo;
+
+	export default ToDo;
